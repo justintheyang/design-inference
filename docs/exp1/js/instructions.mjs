478602a90c156d34e6869f5cc771fc4e578a8b51
justintheyang <justintheyang@gmail.com>
2025-05-30 02:30:49 -0700

    start doing instructions

---
+++ docs/exp1/js/instructions.mjs
@@ -0,0 +1,13 @@
+const instruction_pages = [
+    '',
+]
+
+// 1) In this study, your task is to X. something kitchen is designed for. You will be {making judgments}
+// 2) There are kitchens where one or two cooks try to make one of two possible dishes. (gif of cooks doing their thing)
+// 3)
+
+export const instructions = {
+    type: jsPsychInstructions,
+    pages: instruction_pages,
+    show_clickable_nav: true
+}
