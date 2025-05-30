8f2d8fa74865013f2744813f2c5ebb01241de048
justintheyang <justintheyang@gmail.com>
2025-05-30 05:15:24 -0700

    cursed singleton logic

---
+++ docs/exp1/js/jspsych-singleton.mjs
@@ -0,0 +1,16 @@
+let jsPsych = null;
+
+export function getJsPsych() {
+  if (!jsPsych) {
+    jsPsych = initJsPsych({
+      on_start: () => {
+        settings.session_data.startExperimentTS = Date.now();
+      },
+      on_trial_finish: (data) => {
+        console.log("trial data", data);
+      },
+      show_progress_bar: true,
+    });
+  }
+  return jsPsych;
+}
